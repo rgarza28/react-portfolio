778b22dc92bfb0381ed174393c63533296ef66ef
Robert Garza <garza.robert28@gmail.com>
2020-08-20 00:53:11 -0500

    Updates

---
+++ precache-manifest.00b13b6326b9d66ca1eabab06e484156.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96ea737e8fb94286812f58b706bc375",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b65f3dbfe8d0909dfbf5",
+    "url": "/react-portfolio/static/css/main.b4924a67.chunk.css"
+  },
+  {
+    "revision": "625b3f84f353d174de8d",
+    "url": "/react-portfolio/static/js/2.f4dd564d.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/react-portfolio/static/js/2.f4dd564d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b65f3dbfe8d0909dfbf5",
+    "url": "/react-portfolio/static/js/main.eff43648.chunk.js"
+  },
+  {
+    "revision": "4ac9ecf9d5bb69a1c883",
+    "url": "/react-portfolio/static/js/runtime-main.0f50265a.js"
+  },
+  {
+    "revision": "05cce3b934ef8514001c2489a998dc50",
+    "url": "/react-portfolio/static/media/avatar.05cce3b9.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/react-portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  }
+]);
