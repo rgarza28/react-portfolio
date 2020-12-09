8115c946bd1d347d674ef5828427c3314418060b
Robert Garza <garza.robert28@gmail.com>
2020-12-08 19:38:24 -0600

    updated resume component

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -32,6 +32,76 @@ const useStyles = makeStyles((theme) => ({
       },
     },
   },
+
+  timeLineItem: {
+    padding: "1rem",
+    borderBottom: "2px solid tan",
+    position: "relative",
+    margin: "1rem 3rem 1rem 1rem",
+    clear: "both",
+    "&:after": {
+      content: "''",
+      position: "absolute",
+    },
+    "&:before": {
+      content: "''",
+      position: "absolute",
+      right: "-0.625rem",
+      top: "calc(50% - 5px)",
+      borderStyle: "solid",
+      borderColor: "tomato tomato transparent tranparent",
+      borderWidth: "0.625rem",
+      transform: "rotate(45deg)",
+    },
+    [theme.breakpoints.up("md")]: {
+      width: "44%",
+      margin: "1rem",
+      "&:nth-of-type(2n)": {
+        float: "right",
+        margin: "1rem",
+        borderColor: "tan",
+      },
+      "&:nth-oftype(2n):before": {
+        right: "auto",
+        left: "-0.625rem",
+        borderColor: "transparent transparent tomato tomato",
+      },
+    },
+  },
+  timeLineYear: {
+    textAlign: "center",
+    maxWidth: "9.375rem",
+    margin: "0 3rem 0 auto",
+    fontSize: "1.8rem",
+    background: "tomato",
+    color: "white",
+    lineHeight: 1,
+    padding: "0.5rem 0 1rem",
+    "&:before": {
+      display: "none",
+    },
+    [theme.breakpoints.up("md")]: {
+      textAlign: "center",
+      margin: "0 auto",
+      "&:nth-of-type(2n)": {
+        float: "none",
+        margin: "0 auto",
+      },
+      "&:nth-of-type(2n):before": {
+        display: "none",
+      },
+    },
+  },
+  heading: {
+    color: "tomato",
+    padding: "3rem 0",
+    textTransform: "uppercase",
+  },
+  subHeading: {
+    color:"white",
+    padding:"0",
+    textTransform: "uppercase"
+  }
 }));
 
 const Resume = () => {
@@ -39,13 +109,29 @@ const Resume = () => {
   return (
     <>
       <Navbar />
-      <h2>Resume Component</h2>
+      {/* <h2>Resume Component</h2> */}
       <Box component="header" className={classes.mainContainer}>
-        <Typography variant="h4" align="center">
+        <Typography variant="h4" align="center" className={classes.heading}>
           Work Experience
         </Typography>
         <Box component="div" className={classes.timeLine}>
-          <Typography variant="h2">2019</Typography>
+          <Typography
+            variant="h2"
+            className={`${classes.timeLineYear} ${classes.timeLineItem}`}
+          >
+            2018 - Present
+          </Typography>
+          <Box component="div" className={classes.timeLineItem}>
+            <Typography variant="h5" align="center" className={classes.subHeading}>
+              General Manager
+            </Typography>
+            <Typography variant="body1" align="center" style={{color: "tomato"}}>
+              Werx Repair Services
+            </Typography>
+            <Typography variant="subtitle1" align="center" style={{color: "tan"}}>
+            Manage the repair center, warehouse, quality control, customer service, and product testing aspects of the business. Create documentation for procedures outlining the tasks each department is responsible for. Establish metrics for each role, monitor they are met, and coach individuals who are not meeting them. Coach mentor employees to learn and hone new skills 
+            </Typography>
+          </Box>
         </Box>
       </Box>
     </>
