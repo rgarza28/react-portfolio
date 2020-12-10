f95281eac57b62e31090d94fae957429a883e61a
Robert Garza <garza.robert28@gmail.com>
2020-12-10 00:11:24 -0600

    Updates

---
+++ precache-manifest.d1aa5db70f4485cfa59314fac4bda44c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b7c1894b6565dddf05392fa94d100b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "897154c6cacacb43dc1d",
+    "url": "/react-portfolio/static/css/main.a59e53f5.chunk.css"
+  },
+  {
+    "revision": "b98082481ca4b80c5cce",
+    "url": "/react-portfolio/static/js/2.21f377bf.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/react-portfolio/static/js/2.21f377bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "897154c6cacacb43dc1d",
+    "url": "/react-portfolio/static/js/main.3217c8c5.chunk.js"
+  },
+  {
+    "revision": "4ac9ecf9d5bb69a1c883",
+    "url": "/react-portfolio/static/js/runtime-main.0f50265a.js"
+  },
+  {
+    "revision": "bcb163e282614e4b2260a9230b7016b1",
+    "url": "/react-portfolio/static/media/avatar.bcb163e2.png"
+  },
+  {
+    "revision": "b170a2e46334ecd064e50ea77747ca7e",
+    "url": "/react-portfolio/static/media/bng.b170a2e4.png"
+  },
+  {
+    "revision": "66120e4cf60f60f6f8b411d3d3521e80",
+    "url": "/react-portfolio/static/media/codeQuiz.66120e4c.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/react-portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "99243e7f3103e5fcd0e5c47642a87e5a",
+    "url": "/react-portfolio/static/media/employeeDirectory.99243e7f.png"
+  },
+  {
+    "revision": "1c4fef76c7f44ed53947484a0b0828c6",
+    "url": "/react-portfolio/static/media/inventorymanager.1c4fef76.png"
+  },
+  {
+    "revision": "b9681f6bfefdf8bc2d20a21d1e7117f1",
+    "url": "/react-portfolio/static/media/noteTaker.b9681f6b.png"
+  },
+  {
+    "revision": "bfd9f75df47489be3528ffbe5ccf3162",
+    "url": "/react-portfolio/static/media/travelpacker.bfd9f75d.png"
+  },
+  {
+    "revision": "538c409794a7016948c53f20377f52db",
+    "url": "/react-portfolio/static/media/weatherDash.538c4097.png"
+  }
+]);
